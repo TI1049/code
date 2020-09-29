ad218a05401860f37909044eb652fba6999ee096
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-09-29 17:04:06 +0800

    update 第十七届中国计量大学程序设计竞赛 g

--- 第十七届中国计量大学程序设计竞赛/g/g.cpp
+++ 第十七届中国计量大学程序设计竞赛/g/g.cpp
@@ -31,12 +31,12 @@ template <template<typename...> class T, typename t, typename... A>
 void pt(const T <t> &arg, const A&... args) { for (int i = 0, sze = arg.size(); i < sze; ++i) cout << arg[i] << " \n"[i == sze - 1]; pt(args...); }
 inline ll qpow(ll base, ll n) { assert(n >= 0); ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
 //head
-constexpr int N = 2e5 + 10; 
+constexpr int N = 1e5 + 10; 
 constexpr int S = 450;
 int n, m, q, a[N], f[N], id[N], fid[N], sze[N]; 
 bool isBig[N];
 vector <vector<int>> G, T, H;
-bitset <N> b[N / S + 10];
+bitset <N> b[2 * (N / S) + 10];
 
 void run() {
 	rd(n, m);
@@ -49,24 +49,28 @@ void run() {
 		G[u].push_back(v);
 		G[v].push_back(u);
 	}
-	vector <int> big;
 	*id = 0;
-	H.clear();
+	H.clear(); H.resize(1);
 	for (int i = 1; i <= n; ++i) {
 		if (SZ(G[i]) > S) {
-			big.push_back(i);
 			isBig[i] = 1;
 			id[i] = ++*id;
 			fid[*id] = i;
 			sze[*id] = SZ(G[i]) + 5;
 			b[*id].set();
-			H.push_back(vector<int>(sze[*id]));
+			H.push_back(vector<int>(sze[*id], 0));
 		}
 	}
 	for (int u = 1; u <= n; ++u) {
 		for (auto &v : G[u]) {
 			if (isBig[v]) {
-				T[u].push_back(id[v]);
+				int _v = id[v];
+				T[u].push_back(_v);
+				int x = a[u];
+				if (x < sze[_v]) {
+					++H[_v][x];
+					if (H[_v][x] == 1) b[_v][x] = 0;
+				}
 			}
 		}
 	}
@@ -75,35 +79,36 @@ void run() {
 		rd(op, u);
 		if (op == 1) {
 			rd(x);
-			int pre = a[u];
-			a[u] = x;
-			for (auto &it : T[u]) {
-				if (x <= sze[it]) {
-					--H[it][pre];
-					if (H[it][pre] == 0) b[it][pre] = 1;
-					++H[it][x];
-					if (H[it][x] == 1) b[it][x] = 0;
-				}
-			}
 		} else {
-			int res = n + 1;
 			if (isBig[u]) {
-				res = b[id[u]]._Find_first();
+				x = b[id[u]]._Find_first();
 			} else {
-				int _sze = SZ(G[u]) + 1;
+				int _sze = SZ(G[u]) + 5;
 				for (int i = 0; i <= _sze; ++i) f[i] = 0;
 				for (auto &v : G[u]) {
-					if (a[v] <= _sze) {
-						++f[a[v]];
+					if (a[v] < _sze) {
+						f[a[v]] = 1;
 					}
 				}
 				for (int i = 0; i <= _sze; ++i) if (!f[i]) {
-					res = i;
+					x = i;
 					break;
 				}
 			}	
-			pt(res);
+			pt(x);
 		}	
+		int pre = a[u];
+		a[u] = x;
+		for (auto &it : T[u]) {
+			if (pre < sze[it]) {
+				--H[it][pre];
+				if (H[it][pre] == 0) b[it][pre] = 1;
+			}
+			if (x < sze[it]) {
+				++H[it][x];
+				if (H[it][x] == 1) b[it][x] = 0;
+			}
+		}
 	} 
 }
 
